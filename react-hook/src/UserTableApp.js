2b5564edea9686a2567dde86dc0573c80ed73ef1
joohe71 <joohe71@gmail.com>
2022-05-26 16:53:35 +0900

    Refactor : useReduce 훅 사용하여 리팩토링 연습

--- react-hook/src/UserTableApp.js
+++ react-hook/src/UserTableApp.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, useReducer } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import UserTable from "./UserTable";
@@ -26,55 +26,130 @@ const checkboxes = [
   },
 ];
 
+const initialState = { users: [], searchData: [], filters: {} };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "init": {
+      const { users } = action.payload;
+      return { ...state, users, searchData: users };
+    }
+    case "search": {
+      const { query } = action.payload;
+      const { searchData, filters } = state;
+
+      if (!query) {
+        return { ...state, users: searchData };
+      }
+
+      const pathFn = (item) => {
+        return Object.values(filters).reduce((acc, fn) => {
+          acc.push(fn(item));
+          return acc;
+        }, []);
+      };
+
+      const filteredUsers = searchData.filter((user) => {
+        return (
+          pathFn(user)
+            .map((str) => str.toLowerCase())
+            .join()
+            .search(query) !== -1
+        );
+      });
+
+      return { ...state, users: filteredUsers };
+    }
+    case "add.filter": {
+      const { name, pathFn } = action.payload;
+      return { ...state, filters: { ...state.filters, [name]: pathFn } };
+    }
+    case "remove.filter": {
+      const { name } = action.payload;
+      const { [name]: _, ...rest } = state.filters;
+      return { ...state, filters: rest };
+    }
+    default:
+      return state;
+  }
+};
+
 export default function UserTableApp() {
   // 데이터를 필터링 하는 코드를 구현해보세요.
   // searchData - search에 기반이 되는 리스트입니다.
-  const [searchData, setSearchData] = useState([]);
+  // const [searchData, setSearchData] = useState([]);
   // users - query를 기준으로 search하여 결과를 저장하는 리스트입니다.
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
   // query - 유저의 입력입니다.
   const [query, setQuery] = useState("");
   // filters - filter name에 따른 pathFn을 저장하는 맵입니다.
-  const [filters, setFilters] = useState({});
+  // const [filters, setFilters] = useState({});
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // const handleCheckboxChange = (pathFn) => (e) => {
+  //   const name = e.target.name;
+  //   if (e.target.checked) {
+  //     // filter를 추가합니다.
+  //     return setFilters((filterObj) => ({ ...filterObj, [name]: pathFn }));
+  //   }
+
+  //   // filter를 제거합니다.
+  //   setFilters(({ [name]: _, ...rest }) => rest);
+  // };
+
+  // useEffect(() => {
+  //   axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+  //     const fetchedUsers = res.data;
+  //     setUsers(fetchedUsers);
+  //     setSearchData(fetchedUsers);
+  //   });
+  // }, []);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => dispatch({ type: "init", payload: { users: res.data } }));
+  }, []);
+
+  // useEffect(() => {
+  //   // query가 바뀔 때 서치 로직을 구현하세요.
+  //   if (!query) {
+  //     return setUsers(searchData);
+  //   }
+
+  //   const transformUser = (user) => {
+  //     return Object.values(filters)
+  //       .map((fn) => fn(user)) // ['City']
+  //       .map((str) => str.toLowerCase()) // ['city']
+  //       .join();
+  //   };
+
+  //   const isUserQualified = (user) => transformUser(user).search(query) !== -1;
+
+  //   const filteredUsers = searchData.filter(isUserQualified);
+
+  //   setUsers(filteredUsers);
+  // }, [query, filters, searchData]);
+  useEffect(() => {
+    dispatch({ type: "search", payload: { query } });
+  }, [query]);
+  const handleReset = () => setQuery("");
 
   const handleCheckboxChange = (pathFn) => (e) => {
     const name = e.target.name;
-    if (e.target.checked) {
-      // filter를 추가합니다.
-      return setFilters((filterObj) => ({ ...filterObj, [name]: pathFn }));
-    }
 
-    // filter를 제거합니다.
-    setFilters(({ [name]: _, ...rest }) => rest);
+    if (e.target.checked) {
+      // setFilters((filterObj) => ({ ...filterObj, [name]: pathFn }));
+      dispatch({ type: "add.filter", payload: { name, pathFn } });
+      return;
+    } else {
+      // setFilters(({ [name]: identifier, ...rest }) => rest);
+      dispatch({ type: "remove.filter", payload: { name } });
+    }
+    dispatch({ type: "search", payload: { query } });
   };
 
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-      const fetchedUsers = res.data;
-      setUsers(fetchedUsers);
-      setSearchData(fetchedUsers);
-    });
-  }, []);
-
-  useEffect(() => {
-    // query가 바뀔 때 서치 로직을 구현하세요.
-    if (!query) {
-      return setUsers(searchData);
-    }
-
-    const transformUser = (user) => {
-      return Object.values(filters)
-        .map((fn) => fn(user)) // ['City']
-        .map((str) => str.toLowerCase()) // ['city']
-        .join();
-    };
-
-    const isUserQualified = (user) => transformUser(user).search(query) !== -1;
-
-    const filteredUsers = searchData.filter(isUserQualified);
-
-    setUsers(filteredUsers);
-  }, [query, filters, searchData]);
+  const { users } = state;
 
   return (
     <Container>
@@ -87,13 +162,7 @@ export default function UserTableApp() {
           value={query}
           onChange={(e) => setQuery(e.target.value.trim().toLowerCase())}
         />
-        <button
-          type="button"
-          onClick={() => {
-            setQuery("");
-            setFilters({});
-          }}
-        >
+        <button type="button" onClick={handleReset}>
           Reset
         </button>
       </div>
