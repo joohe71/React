6dd53e0fcab43cc9a896b92f22545c519e8f25d1
joohe71 <joohe71@gmail.com>
2022-06-05 23:32:15 +0900

    Practice : 테스트 코드 짜면서 구현 완료하기

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SimpleToggle() {
-  return <button>유저정보 보기</button>;
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow((prev) => !prev);
+  return (
+    <div>
+      {!show && <div>유저 정보를 보려면 버튼을 누르세요.</div>}
+      {show && (
+        <ul>
+          <li>Email - test@test.com</li>
+          <li>Address - 서울시 강남구</li>
+        </ul>
+      )}
+      <button onClick={handleClick}>
+        {!show ? "유저정보 보기" : "유저정보 가리기"}
+      </button>
+    </div>
+  );
 }
 
 export default SimpleToggle;
--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -1,5 +1,6 @@
 import { screen, render } from "@testing-library/react";
 import SimpleToggle from "./App";
+import userEvent from "@testing-library/user-event";
 
 describe("앱을 렌더링합니다.", () => {
   test("버튼이 있습니다.", () => {
@@ -19,34 +20,45 @@ describe("앱을 렌더링합니다.", () => {
     // 텍스트 - "유저 정보를 보려면 버튼을 누르세요."
     // screen.getByText를 이용합니다.
     // 텍스트가 존재하는지 체크합니다.
+    const text = screen.getByText("유저 정보를 보려면 버튼을 누르세요.");
+    expect(text).toBeInTheDocument();
   });
 });
 
 describe("토글 기능을 테스트합니다.", () => {
   test("버튼을 눌렀을 시, 유저 정보가 보입니다.", () => {
     render(<SimpleToggle />);
+    const infoText = "유저 정보를 보려면 버튼을 누르세요.";
 
     // 텍스트를 찾습니다.
     // 텍스트 - "유저 정보를 보려면 버튼을 누르세요."
     // 텍스트가 존재하는지 체크합니다.
+    const text = screen.getByText(infoText);
+    expect(text).toBeInTheDocument();
 
     // Toggle 버튼을 찾습니다.
     // 버튼을 클릭합니다.
     // 위에서 찾은 텍스트가 보이지 않는지 체크합니다.
     // screen.queryByText를 이용합니다.
+    const button = screen.getByRole("button", { name: "유저정보 보기" });
+    userEvent.click(button);
+    expect(screen.queryByText(infoText)).not.toBeInTheDocument();
 
     // 이메일 정보를 찾습니다.
-    // 이메일 정보 - "Email - elice@elicer.com"
     // screen.getByText를 이용합니다.
     // 이메일 정보가 문서에 존재하는지 체크합니다.
+    const email = screen.getByText("Email - test@test.com");
+    expect(email).toBeInTheDocument();
 
     // 주소 정보를 찾습니다.
-    // 주소 정보 - "Address - 서울시 강남구 테헤란로 401"
     // screen.getByText를 이용합니다.
     // 주소 정보가 문서에 존재하는지 체크합니다.
+    const address = screen.getByText("Address - 서울시 강남구");
+    expect(address).toBeInTheDocument();
 
     // 버튼의 텍스트가 "유저정보 가리기" 로 바뀌는지 체크합니다.
     // toHaveTextContent matcher를 이용합니다.
+    expect(button).toHaveTextContent("유저정보 가리기");
   });
 
   test("버튼을 두번 누르면, 유저 정보가 보이지 않습니다.", () => {
@@ -56,9 +68,17 @@ describe("토글 기능을 테스트합니다.", () => {
     // 버튼을 클릭합니다.
     // userEvent.click을 이용합니다.
     // 이메일 정보가 문서에 있는지 체크합니다.
+    const button = screen.getByRole("button", {
+      name: "유저정보 보기",
+    });
+    userEvent.click(button, { clickCount: 1 });
+    const email = screen.getByText("Email - test@test.com");
+    expect(email).toBeInTheDocument();
 
     // Toggle 버튼을 클릭합니다.
     // 이메일 정보가 문서에서 사라졌는지 체크합니다.
     // screen.queryByText를 이용합니다.
+    userEvent.click(button, { clickCount: 1 });
+    expect(email).not.toBeInTheDocument();
   });
 });
